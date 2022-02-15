612a43f3df85ad910df99d265909080b09cb1a16
Fede Gomez <gomez.fede98@gmail.com>
2022-02-15 09:49:59 -0300

    Slider updated

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,37 +1,50 @@
-import React from 'react';
-import './Slider.scss';
+import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper';
+import { Navigation, Pagination, Thumbs } from 'swiper';
+import './Slider.scss';
 import 'swiper/scss';
 import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
-import 'swiper/scss/scrollbar';
+import 'swiper/scss/thumbs';
+//Images
 import img1 from '../assets/image-product-1.jpg';
 import img2 from '../assets/image-product-2.jpg';
 import img3 from '../assets/image-product-3.jpg';
 import img4 from '../assets/image-product-4.jpg';
+//Thumbnails
+import thumb1 from '../assets/image-product-1-thumbnail.jpg';
+import thumb2 from '../assets/image-product-2-thumbnail.jpg';
+import thumb3 from '../assets/image-product-3-thumbnail.jpg';
+import thumb4 from '../assets/image-product-4-thumbnail.jpg';
 
 const Slider = () => {
+	const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
 	return (
 		<div>
 			<Swiper
+				style={{
+					'--swiper-navigation-color': '#fff',
+					'--swiper-pagination-color': '#fff',
+				}}
 				modules={[Navigation, Pagination]}
 				spaceBetween={50}
 				slidesPerView={1}
 				navigation
 				pagination={{ clickable: true }}
-				loop={true}>
+				loop={true}
+				className='slider'>
 				<SwiperSlide>
-					<img className='slide' src={img1} alt='' />
+					<img src={img1} alt='shoes' />
 				</SwiperSlide>
 				<SwiperSlide>
-					<img className='slide' src={img2} alt='' />
+					<img src={img2} alt='shoes' />
 				</SwiperSlide>
 				<SwiperSlide>
-					<img className='slide' src={img3} alt='' />
+					<img src={img3} alt='shoes' />
 				</SwiperSlide>
 				<SwiperSlide>
-					<img className='slide' src={img4} alt='' />
+					<img src={img4} alt='shoes' />
 				</SwiperSlide>
 			</Swiper>
 		</div>
